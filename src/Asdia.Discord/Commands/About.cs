8173735be1f117b7426efbd741b11d7778ec49e2
asdia0 <asdia.the.potato@gmail.com>
2021-05-22 23:36:23 +0800

    Add Commands.About

---
+++ src/Asdia.Discord/Commands/About.cs
@@ -0,0 +1,16 @@
+﻿namespace Asdia.Discord.Commands
+{
+    using System.Threading.Tasks;
+    using DSharpPlus.CommandsNext;
+    using DSharpPlus.CommandsNext.Attributes;
+
+    public class About : BaseCommandModule
+    {
+        [Command("ping")]
+        [Aliases("p")]
+        public async Task PingCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"{ctx.Client.Ping}ms").ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Asdia.Discord/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Asdia.Discord
+{
+    using Asdia.Discord.Commands;
+    using System.IO;
+    using System.Threading.Tasks;
+    using DSharpPlus;
+    using DSharpPlus.CommandsNext;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = File.ReadAllText("token.txt"),
+                TokenType = TokenType.Bot
+            });
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "a!" }
+            });
+
+            commands.RegisterCommands<About>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+    }
+}
