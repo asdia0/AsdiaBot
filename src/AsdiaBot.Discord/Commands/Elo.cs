64c8c8d5092f7e2a37358942ef584fd02b3a1ecb
asdia0 <asdia.the.potato@gmail.com>
2021-07-07 18:49:10 +0800

    Remove unneccessary using directives

--- src/AsdiaBot.Discord/Commands/Elo.cs
+++ src/AsdiaBot.Discord/Commands/Elo.cs
@@ -1,18 +1,11 @@
 ﻿namespace AsdiaBot.Discord.Commands
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.IO;
-    using System.Threading.Tasks;
     using DSharpPlus.CommandsNext;
     using DSharpPlus.CommandsNext.Attributes;
-    using DSharpPlus.Entities;
-    using Newtonsoft.Json;
 
     [Group("Elo")]
     public class Elo : BaseCommandModule
     {
-        
+
     }
 }
