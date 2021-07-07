2360c605e0a39914e549c91b79241a024b345177
asdia0 <asdia.the.potato@gmail.com>
2021-07-07 19:57:00 +0800

    Add Elo.Version()

--- src/AsdiaBot.Discord/Commands/Elo/Elo.cs
+++ src/AsdiaBot.Discord/Commands/Elo/Elo.cs
@@ -1,11 +1,19 @@
-﻿namespace AsdiaBot.Discord.Commands
+﻿namespace AsdiaBot.Discord
 {
+    using System.IO;
+    using System.Threading.Tasks;
     using DSharpPlus.CommandsNext;
     using DSharpPlus.CommandsNext.Attributes;
+    using DSharpPlus.Entities;
 
     [Group("Elo")]
     public class Elo : BaseCommandModule
     {
-
+        [Command("version")]
+        [Description("Gets the EloCalculator version running.")]
+        public async Task Version(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("EloCalculator v1.0.0");
+        }
     }
 }
