836b0f968463162c9b7421454d6beb8cd7df4a39
asdia0 <asdia.the.potato@gmail.com>
2021-07-07 19:14:20 +0800

    Add Game.AddGame()

--- src/AsdiaBot.Discord/Commands/Elo/Game.cs
+++ src/AsdiaBot.Discord/Commands/Elo/Game.cs
@@ -1,7 +1,80 @@
-﻿namespace AsdiaBot.Discord.Commands.Elo
+﻿namespace AsdiaBot.Discord
 {
-    public class Game
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DSharpPlus.CommandsNext;
+    using DSharpPlus.CommandsNext.Attributes;
+    using DSharpPlus.Entities;
+    using EloCalculator;
+
+    [Group("Game")]
+    public class Game : Elo
     {
-        
+        [Command("add")]
+        [Description("Adds a game.")]
+        public async Task AddGame(CommandContext ctx, string whiteName, string blackName, string gameResult, string isRated)
+        {
+            if (!ctx.Member.Roles.Contains(ChessAdmin))
+            {
+                await ctx.Channel.SendMessageAsync("Only chess admins can add games.");
+            }
+
+            Player white = PlayerDatabase.Players.Where(i => i.Name == whiteName).FirstOrDefault();
+            Player black = PlayerDatabase.Players.Where(i => i.Name == blackName).FirstOrDefault();
+            Result? result = null;
+            bool? rated = null;
+
+            if (white == null)
+            {
+                await ctx.Channel.SendMessageAsync($"Create player {whiteName} with `elo player add` first.");
+                return;
+            }
+
+            if (black == null)
+            {
+                await ctx.Channel.SendMessageAsync($"Create player {blackName} with `elo player add` first.");
+                return;
+            }
+
+            switch (gameResult)
+            {
+                case "white":
+                    result = Result.White;
+                    break;
+                case "black":
+                    result = Result.Black;
+                    break;
+                case "draw":
+                    result = Result.Draw;
+                    break;
+            }
+
+            if (result == null)
+            {
+                await ctx.Channel.SendMessageAsync($"Result must be  \"white\", \"black\" or \"draw\".");
+                return;
+            }
+
+            switch (isRated)
+            {
+                case "true":
+                    rated = true;
+                    break;
+                case "false":
+                    rated = false;
+                    break;
+            }
+
+            if (rated == null)
+            {
+                await ctx.Channel.SendMessageAsync($"Result must be either \"true\" or \"false\".");
+                return;
+            }
+
+            EloCalculator.Game game = new EloCalculator.Game(white, black, (Result)result, DateTime.Now, (bool)rated);
+
+            await ctx.Channel.SendMessageAsync($"Createed game {game.ID}.");
+        }
     }
 }
